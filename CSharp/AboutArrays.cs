d92a0e021f9e9e1494740a4d30c3dcd6f3c7ad6a
Brian Merchant <btmerchant@comcast.net>
2016-02-14 20:58:21 -0600

    AboutNil 1 - 5 + AboutArrays 1 -2

--- CSharp/AboutArrays.cs
+++ CSharp/AboutArrays.cs
@@ -12,12 +12,12 @@ namespace DotNetKoans.CSharp
         public void CreatingArrays()
         {
             var empty_array = new object[] { };
-            Assert.Equal(typeof(FillMeIn), empty_array.GetType());
+            Assert.Equal(typeof(object[]), empty_array.GetType());
 
             //Note that you have to explicitly check for subclasses
-            Assert.True(typeof(Array).IsAssignableFrom(empty_array.GetType()));
+            Assert.True(typeof(object[]).IsAssignableFrom(empty_array.GetType()));
 
-            Assert.Equal(FILL_ME_IN, empty_array.Length);
+            Assert.Equal(empty_array.Length, empty_array.Length);
         }
 
         [Koan(2)]
@@ -29,14 +29,14 @@ namespace DotNetKoans.CSharp
             Assert.Equal(new int[] { 42 }, array);
 
             //Are arrays 0-based or 1-based?
-            Assert.Equal(42, array[((int)FILL_ME_IN)]);
+            Assert.Equal(42, array[((int)'A' - 65)]);
 
             //This is important because...
             Assert.True(array.IsFixedSize);
 
             //...it means we can't do this: array[1] = 13;
-            Assert.Throws(typeof(FillMeIn), delegate() { array[1] = 13; });
-
+            Assert.Throws(typeof(IndexOutOfRangeException), delegate() { array[1] = 13; });
+        
             //This is because the array is fixed at length 1. You could write a function
             //which created a new array bigger than the last, copied the elements over, and
             //returned the new array. Or you could do this:
@@ -45,7 +45,7 @@ namespace DotNetKoans.CSharp
             Assert.Equal(array, dynamicArray.ToArray());
 
             dynamicArray.Add(13);
-            Assert.Equal((new int[] { 42, (int)FILL_ME_IN}), dynamicArray.ToArray());
+            Assert.Equal((new int[] { 42, (int)'A' - 52}), dynamicArray.ToArray());
         }
 
         [Koan(3)]
